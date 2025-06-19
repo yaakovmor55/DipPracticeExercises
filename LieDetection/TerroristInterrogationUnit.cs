606e4221c74360e790ffa2fe8c6abe89d40bafc7
Yaakov <y0583275590@gmail.com>
2025-06-19 16:25:00 +0300

    I added exercise 2.

---
+++ LieDetection/TerroristInterrogationUnit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DipPracticeExercises.LieDetection
+{
+    internal class TerroristInterrogationUnit
+    {
+        private ILieDetector detector;
+
+        public TerroristInterrogationUnit(ILieDetector detector)
+        {
+            this.detector = detector;
+        }
+    }
+
+    public void Interrogate(string sample)
+        {
+            bool isLying = detector.IsLying(sample);
+
+            if (isLying)
+            {
+                Console.WriteLine("Suspect is lying!");
+            }
+            else
+            {
+                Console.WriteLine("Suspect is telling the truth.");
+            }
+        }
+}
---
+++ LieDetection/ThermalScanner .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DipPracticeExercises.LieDetection
+{
+    internal class ThermalScanner : ILieDetector
+    {
+        public bool IsLying(string data)
+        {
+            return data.Contains("heat");
+        }
+    }
+}
